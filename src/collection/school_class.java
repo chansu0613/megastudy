332b3c7f3f86520ea738e01859908a437ee36274
chan <b2451184@naver.com>
2022-10-23 13:56:48 +0900

    lolpro

---
+++ src/collection/school_class.java
@@ -0,0 +1,13 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class school_class {
+    List students;
+
+    school_class(){
+        students = new ArrayList();
+
+    }
+}
---
+++ src/collection/test.java
@@ -0,0 +1,50 @@
+package collection;
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        HashMap myList = new HashMap();
+        List toplist = new ArrayList();
+        List junglelist = new ArrayList();
+        List midlist = new ArrayList();
+        List botlist = new ArrayList();
+        List sptlist = new ArrayList();
+
+
+        myList.put("top",toplist);
+        myList.put("jg",junglelist);
+        myList.put("mid",midlist);
+        myList.put("bot",botlist);
+        myList.put("spt",sptlist);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Champ : ");
+        String champion = sc.nextLine();
+        System.out.print("Position : ");
+        String position = sc.nextLine();
+
+        List list = (List) myList.get(position);
+        list.add(champion);
+
+        System.out.println(myList);
+
+        System.out.print("Champ : ");
+        champion = sc.nextLine();
+        System.out.print("Position : ");
+        position = sc.nextLine();
+
+        if (!Arrays.asList(myList).contains(position)) {
+            list = (List) myList.get(position);
+            list.add(champion);
+            System.out.println(myList);
+        }
+        continue;
+
+
+
+
+
+
+    }
+}
