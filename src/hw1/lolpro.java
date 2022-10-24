458f44a28845ddb131a539450e879422503c86a7
chansu <b2451184@naver.com>
2022-10-25 01:10:27 +0900

    lol-testing

---
+++ src/hw1/lolpro.java
@@ -0,0 +1,45 @@
+package hw1;
+
+import java.util.*;
+
+public class lolpro {
+
+public class test {
+    public static void main(String[] args) {
+        HashMap myList = new HashMap();
+        List toplist = new ArrayList();
+        List junglelist = new ArrayList();
+        List midlist = new ArrayList();
+        List botlist = new ArrayList();
+        List sptlist = new ArrayList();
+
+
+        myList.put("top",toplist);
+        myList.put("jg",junglelist);
+        myList.put("mid",midlist);
+        myList.put("bot",botlist);
+        myList.put("spt",sptlist);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Champ : ");
+        String champion = sc.nextLine();
+        System.out.print("Position : ");
+        String position = sc.nextLine();
+
+        List list = (List) myList.get(position);
+        list.add(champion);
+
+        System.out.println(myList);
+
+        System.out.print("Champ : ");
+        champion = sc.nextLine();
+        System.out.print("Position : ");
+        position = sc.nextLine();
+
+        if (!Arrays.asList(myList).contains(position)) {
+            list = (List) myList.get(position);
+            list.add(champion);
+            System.out.println(myList);
+        }
+    }
+}
